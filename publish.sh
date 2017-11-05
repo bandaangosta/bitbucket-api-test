45c4aa08c5ac589ac1d16ce9b213a200d313759d
Jose Ortiz <jlortiz@uc.cl>
2017-11-05 12:47:09 -0300

    Update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
-mv slides.slides.html  index.html
+#jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
+jupyter-nbconvert --to slides bitbucket-api-xml-read.ipynb --reveal-prefix=reveal.js
+#mv slides.slides.html index.html
+mv  bitbucket-api-xml-read.slides.html index.html
 mkdir -p /tmp/workspace
 cp -r * /tmp/workspace/
 git add -A .
